f6ad295e7dc12afb2dadd884975ff9fd96071dca
Crejak <crejak.dev@gmail.com>
2016-11-04 20:18:09 +0100

    test resize javascript

--- js/test.js
+++ js/test.js
@@ -6,6 +6,9 @@ window.onload = function () {
   canvas = document.getElementById("screen");
   ctx = canvas.getContext("2d");
 
+  ctx.canvas.width = window.innerWidth;
+  ctx.canvas.height = window.innerHeight;
+
   ctx.strokeRect(40, 40, 80, 40);
   ctx.fillText("Hello World !", 50, 65);
 };
